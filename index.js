81794673c48d1f60240ba02fdecd1f8d874efcad
Nattalic <nordonez376@gmail.com>
2025-08-20 07:49:44 -0500

    creating notes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Objetos 
+//POO  
 
 const persona = {
     nombre: "juan",
@@ -7,8 +7,8 @@ const persona = {
 }
 
 //clase de objetos -- armar objetos que tengan un molde
+//una clase es aquella que define caracteristicas y comportamientos que tendran todos los objetos creados a partir de el
 //una clase genera objetos
-//constructor es un elemento dentro de las clases que permite configurar aspectos iniciales dentro de un objeto
 
 
 //privado publico y protegido
@@ -16,7 +16,7 @@ const persona = {
 // getters: obtener la info 
 // setters: como puedo actualizar la info
 
-
+//constructor es un elemento dentro de las clases que permite configurar aspectos iniciales dentro de un objeto
 class Persona {
     constructor(nombre,edad,profesion) {
         this.nombre = nombre 
@@ -25,7 +25,7 @@ class Persona {
         this._password = 12345 // se le puede agregar un _password para que sean tratados como semi-privados
     }
 
-
+    //Metodos: dentro de las clases representen algo que especificamente puedan hacer
     hablar() {
         console.log(`Hola, soy ${this.nombre} y soy ${this.profesion}`)
     }
@@ -42,10 +42,12 @@ class Persona {
     }
 }
 }
-//funciones metodos dentro de las clases representen algo que especificamente puedan hacer
 
 
-//instancial, apartir de un molde estoy creando un elemento especifico de ese tipo
+
+//instancia: apartir de un molde estoy creando un elemento especifico de ese tipo
+//es un objeto real creado a partir de una clase
+//todos los objetos comparten la misma estructura y comportamientos sin embargo cada uno tiene sus propios valores y estado
 
 const juanPablo = new Persona ("Juan Pablo", 18, "Ingeniero y Diseñador :3")
 const natalia = new Persona ("Natalia", 20, "Diseñadora")
@@ -54,13 +56,17 @@ console.log(juanPablo.hablar()) //acceder a metodos de funciones
 
 juanPablo.edad = -30
 console.log(juanPablo)
-//crear objetos por medio de clases tenemos moldes, todos los objetos dentro de la aplicacion que sean personas siempre tendran
-//las mismas caracterisiticas
+
 
 //super: super clase, clase de la que yo estoy puesta, ejecuta el constructor de la persona
+
+
+//CONCEPTOS
 
 //polimorfismo: multiples formas de hacer las cosas 
 //herencia: heredar diferentes atributos, caracteristicas
 //abstraccion: busca que los aspectos complejos (pasos) solamente se haga la accion de una palabra a un nivel simple
 //solo hacer la accion sin necesidad de la complejidad del bloque d codigo 
-//encapsulamiento: 
+
+
+//encapsulamiento:  ???
